7f53c1baed2538bab85599d070e412c48dbaced0
armando <armando.lezama.psic@gmail.com>
2021-10-23 12:22:20 -0500

    feat(js)/added tm and excluded words

--- index.js
+++ index.js
@@ -3,27 +3,31 @@
  * uncoment this line for fast local development
  */
 
+const tm = require( 'text-miner' );
+const excludedWords = require('./src/excluded-words');
 const sentimentAnalyzer = require('./src/sentiment');
 let sentimentReport = [];
 
-const getReport = data => {
+const getReport = (data, freq) => {
   for (const tweetRecorded of data) {
     sentimentReport = [...sentimentReport, sentimentAnalyzer(tweetRecorded['tweet']['text'])]
   };
   
   const report = sentimentReport.reduce((acc, curr)=>{
+    const texts = curr.tokens.filter(text => {
+      return !text.includes('@') && text.length > 1 && !excludedWords.includes(text);
+    });
     return {
       score: acc.score + curr.score,
-      texts: [...acc.texts, ...curr.tokens],
-      goodTexts: curr.score > 0 ? [...acc.goodTexts, ...curr.tokens] : acc.goodTexts,
-      badTexts: curr.score > 0 ? [...acc.texts, ...curr.tokens] : acc.texts,
+      texts: [...acc.texts, ...texts],
+      goodTexts: curr.score > 0 ? [...acc.goodTexts, ...texts] : acc.goodTexts,
+      badTexts: curr.score < 0 ? [...acc.badTexts, ...texts] : acc.badTexts,
       goodScore: curr.score > 0 ? acc.goodScore + curr.score : acc.goodScore,
       badScore: curr.score < 0 ? acc.badScore + curr.score : acc.badScore,
     };
   }, {
     score: 0,
     texts : [],
-    locations: [],
     goodTexts: [],
     badTexts: [],
     goodScore: 0,
@@ -31,6 +35,10 @@ const getReport = data => {
   })
   
   report.mean = report.score/sentimentReport.length;
+  report.trend = report.mean > 0 ? 'good' : report.mean < 0 ? 'bad' : 'neutral';
+  const corpus = new tm.Corpus(report.texts);
+  const terms = new tm.DocumentTermMatrix( corpus );
+  report.frequencies = terms.findFreqTerms( freq );
 
   return report;
 };
